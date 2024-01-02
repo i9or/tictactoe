a61c16b1e260a1ac84bbf55cb9c6230497748945
Igor <dont@bzzr.me>
2024-01-02 19:04:23 +0000

    Update SFML

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,32 @@
+#include <vector>
+
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-  sf::VideoMode videoMode { sf::Vector2u { 300, 300 } };
-  sf::RenderWindow window { videoMode, "Tic Tac Toe!!" };
+constexpr auto WINDOW_SIZE = 300u;
+
+int main() {
+  sf::ContextSettings settings;
+  settings.antialiasingLevel = 8;
+  sf::RenderWindow window{
+      {WINDOW_SIZE, WINDOW_SIZE}, "Tic Tac Toe", sf::Style::Default, settings};
 
   while (window.isOpen()) {
-    sf::Event event {};
+    sf::Event event{};
     while (window.pollEvent(event)) {
       if (event.type == sf::Event::Closed) {
         window.close();
       }
 
-      if (event.type == sf::Event::KeyPressed
-          && (event.key.code == sf::Keyboard::Escape)) {
+      if (event.type == sf::Event::KeyPressed &&
+          (event.key.code == sf::Keyboard::Escape)) {
         window.close();
       }
     }
 
     window.clear(sf::Color::Black);
+    // TODO: Add drawing code here
     window.display();
   }
 
-  return EXIT_SUCCESS;
+  return 0;
 }
